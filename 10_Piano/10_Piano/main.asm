6a441a54d78883bb167448f8b7f60c284396f58d
ArrobaRicardoGE <ricardoarroba@outlook.com>
2021-11-15 09:59:10 -0600

    [Practica 9 y 10] Agregar

---
+++ 10_Piano/10_Piano/main.asm
@@ -0,0 +1,138 @@
+;*******************
+; Práctica 09: Cronómetro
+;
+; Created: 09/11/2021
+; Author : Ricardo Gutiérrez
+;*******************
+
+.include "m16adef.inc"     
+   
+;*******************
+;Registros (aquí pueden definirse)
+.def s=r19
+.def m=r20
+.def h=r21
+.def counting=r22
+
+;Palabras claves (aquí pueden definirse)
+;.equ LCD_DAT=DDRC
+;********************
+
+.org 0x0000
+;Comienza el vector de interrupciones...
+jmp RESET ; Reset Handler
+jmp EXT_INT0 ; IRQ0 Handler
+jmp EXT_INT1 ; IRQ1 Handler
+jmp TIM2_COMP ; Timer2 Compare Handler
+jmp TIM2_OVF ; Timer2 Overflow Handler
+jmp TIM1_CAPT ; Timer1 Capture Handler
+jmp TIM1_COMPA ; Timer1 CompareA Handler
+jmp TIM1_COMPB ; Timer1 CompareB Handler
+jmp TIM1_OVF ; Timer1 Overflow Handler
+jmp TIM0_OVF ; Timer0 Overflow Handler
+jmp SPI_STC ; SPI Transfer Complete Handler
+jmp USART_RXC ; USART RX Complete Handler
+jmp USART_UDRE ; UDR Empty Handler
+jmp USART_TXC ; USART TX Complete Handler
+jmp ADC_COMP ; ADC Conversion Complete Handler
+jmp EE_RDY ; EEPROM Ready Handler
+jmp ANA_COMP ; Analog Comparator Handler
+jmp TWSI ; Two-wire Serial Interface Handler
+jmp EXT_INT2 ; IRQ2 Handler
+jmp TIM0_COMP ; Timer0 Compare Handler
+jmp SPM_RDY ; Store Program Memory Ready Handler
+
+;**************
+;Inicializar el Stack Pointer
+;**************
+Reset:
+ldi r16, high(RAMEND)
+out SPH, r16
+ldi r16, low(RAMEND)
+out SPL, r16 
+
+
+;*********************************
+;Aquí comienza el programa...
+;No olvides configurar al inicio todo lo que utilizarás
+;*********************************
+
+; Configuracion de los puertos
+ldi r16, 255
+out DDRB, r16
+
+; Configuracion del timer
+
+ldi r16, 0b0001_1011	; Prescaler 64, modo CTC, toggle
+out TCCR0, r16
+ldi r16, 29 ; DO
+ldi r16, 26 ; RE
+ldi r16, 23 ; MI
+ldi r16, 21 ; FA
+ldi r16, 19 ; SOL
+ldi r16, 17 ; LA
+ldi r16, 15 ; SI
+out OCR0, r16
+
+main:
+rjmp main
+
+retardo:	
+	ldi r30, 0xA0
+	ciclo1:
+		dec r30
+		breq salir
+		ldi r31, 0xA0
+	ciclo2:
+		dec r31
+		breq ciclo1
+		rjmp ciclo2 
+salir:
+ret
+
+;*********************************
+;Aquí está el manejo de las interrupciones concretas
+;*********************************
+EXT_INT0: ; IRQ0 Handler
+reti
+EXT_INT1: 
+reti ; IRQ1 Handler
+TIM2_COMP: 
+reti ; Timer2 Compare Handler
+TIM2_OVF: 
+reti ; Timer2 Overflow Handler
+TIM1_CAPT: 
+reti ; Timer1 Capture Handler
+TIM1_COMPA: 
+reti ; Timer1 CompareA Handler
+TIM1_COMPB: 
+reti ; Timer1 CompareB Handler
+TIM1_OVF: 
+reti ; Timer1 Overflow Handler
+TIM0_OVF: 
+reti ; Timer0 Overflow Handler
+SPI_STC: 
+reti ; SPI Transfer Complete Handler
+USART_RXC: 
+reti ; USART RX Complete Handler
+USART_UDRE: 
+reti ; UDR Empty Handler
+USART_TXC: 
+reti ; USART TX Complete Handler
+ADC_COMP: 
+reti ; ADC Conversion Complete Handler
+EE_RDY: 
+reti ; EEPROM Ready Handler
+ANA_COMP: 
+reti ; Analog Comparator Handler
+TWSI: 
+reti ; Two-wire Serial Interface Handler
+EXT_INT2: 
+reti ; IRQ2 Handler
+TIM0_COMP:
+reti
+SPM_RDY: 
+reti ; Store Program Memory Ready Handler
+
+
+
