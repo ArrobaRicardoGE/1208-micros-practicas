a8b18e9f82594aea9b160f7d1ed56e9cffa36432
ArrobaRicardoGE <ricardoarroba@outlook.com>
2021-12-02 01:25:35 -0600

    [Examen 3] Resolver el tercer examen

--- E3/E3/main.asm
+++ E3/E3/main.asm
@@ -2,14 +2,30 @@
 ; Examen Final Microcontroladores I
 ;
 ; Created: 30/11/2021
-; Author : Ricardo Antonio Gutiérrez Esparza
+; Author : Ricardo Antonio Gutiérrez Esparza (21)
 ;*******************
+
+/* TABLA VALORES PARA PWM
+0CR0	| Tiempo en alto	| Ángulo	| Valores
+123		| 0.000992			| 0			|  0 -  9		
+127		| 0.001024			| 20		| 10 - 29		
+131		| 0.001056			| 40		| 30 - 49		
+135		| 0.001088			| 60		| 50 - 69		
+139		| 0.001120			| 80		| 70 - 89
+143		| 0.001152			| 100		| 90 - 99				
+
+*/
 
 .include "m16adef.inc"     
    
 ;*******************
 ;Registros (aquí pueden definirse)
-;.def temporal=r19
+.def new=r17		; Guarda el nuevo valor recibido por el teclado
+.def temporal1=r30	; Temporales, usos múltpiles
+.def temporal2=r31
+.def estado=r18		; Guarda el estado en el que se encuentra el sistema. 1, 2 -> leer valor, 3 -> esperando accionar, 4 -> accionado
+.def d_low=r19		; Guarda las unidades del valor deseado
+.def d_high=r20		; Guarda las decenas del valor deseado
 
 ;Palabras claves (aquí pueden definirse)
 ;.equ LCD_DAT=DDRC
@@ -54,16 +70,248 @@ out SPL, r16
 ;No olvides configurar al inicio todo lo que utilizarás
 ;*********************************
 
-;Prescalers
-;001 -> 1
-;010 -> 8
-;011 -> 64
-;100 -> 256
-;101 -> 1024
+; Configuración de PWM
+ldi r16, 0b01_10_1_011	; Prescaler 64, modo FastPWM
+out TCCR0, r16
+ldi r16, 123			; Inicializamos en 0
+out OCR0, r16
 
+; Configuración de puertos
 
+ldi r16, 255			; Salidas
+out DDRB, r16
+out DDRC, r16
+out DDRD, r16
 
+ldi r16, 0b0000_1111	; Filas salidas, columnas entradas (respecto al micro)
+out DDRA, r16			; Puerto A para teclado matricial
+ldi r16, 0b1111_1111	; Teclado con pull up
+out PORTA, r16	
 
+ldi estado, 1			; Inicializamos el estado
+
+main:
+; Lectura de teclado
+ldi new, 10
+;Primera fila
+ldi r16, 0b1111_1110
+out PORTA, r16
+nop
+nop
+nop
+sbis PINA, 6 ;#
+ldi new, 11
+sbis PINA, 5 ;0
+ldi new, 0
+sbis PINA, 4 ;*
+ldi new, 12
+cpi new, 10
+brne procesar
+;Segunda fila
+ldi r16, 0b1111_1101
+out PORTA, r16
+nop
+nop
+nop
+sbis PINA, 6 ;9
+ldi new, 9
+sbis PINA, 5 ;8
+ldi new, 8
+sbis PINA, 4 ;7
+ldi new, 7
+cpi new, 10
+brne procesar
+;Tercera fila
+ldi r16, 0b1111_1011
+out PORTA, r16
+nop
+nop
+nop
+sbis PINA, 6 ;6
+ldi new, 6
+sbis PINA, 5 ;5
+ldi new, 5
+sbis PINA, 4 ;4
+ldi new, 4
+cpi new, 10
+brne procesar
+;Cuarta fila
+ldi r16, 0b1111_0111
+out PORTA, r16
+nop
+nop
+nop
+sbis PINA, 6 ;3
+ldi new, 3
+sbis PINA, 5 ;2
+ldi new, 2
+sbis PINA, 4 ;1
+ldi new, 1
+cpi new, 10
+brne procesar
+rjmp main
+
+procesar:
+cpi new, 11			; Clear (#)
+breq clear
+cpi estado, 4		; Si ya actué, espero clear
+breq proc_to_main
+cpi estado, 3		; Si ya tengo digitos cargados, espero actuar
+brne digitos
+cpi new, 12			; Actuar (*) 
+breq actuar
+rjmp to_main
+digitos:
+cpi new, 10			; Quitar el asterisco
+brge proc_to_main
+mov d_high, d_low
+mov d_low, new
+inc estado
+rjmp output
+proc_to_main:
+rjmp to_main
+
+clear:
+ldi d_low, 0
+ldi d_high, 0
+ldi estado, 1
+ldi r16, 123
+out OCR0, r16
+out PORTC, d_low
+out PORTD, d_high
+rjmp to_main
+
+output:
+; Voltear bits para salida
+ldi temporal1, 0
+sbrc d_low, 0
+inc temporal1
+lsl temporal1
+sbrc d_low, 1
+inc temporal1
+lsl temporal1
+sbrc d_low, 2
+inc temporal1
+lsl temporal1
+sbrc d_low, 3
+inc temporal1
+out PORTC, temporal1
+
+ldi temporal1, 0
+sbrc d_high, 0
+inc temporal1
+lsl temporal1
+sbrc d_high, 1
+inc temporal1
+lsl temporal1
+sbrc d_high, 2
+inc temporal1
+lsl temporal1
+sbrc d_high, 3
+inc temporal1
+out PORTD, temporal1
+rjmp to_main
+
+actuar:
+inc estado
+; En lugar de preguntar por el valor completo, solo me fijo en las decenas
+; ----
+; NOTA: Si se quisiera juntar en un solo registro el valor, aquí es el lugar
+; para hacerlo. Habría que sumar 10 veces d_high y una vez d_low. Mi solución
+; no lo necesita, por eso no lo implementé.
+ldi r16, 0
+cp r16, d_high
+brge cero
+ldi r16, 2
+cp r16, d_high
+brge veinte
+ldi r16, 4
+cp r16, d_high
+brge cuarenta
+ldi r16, 6
+cp r16, d_high
+brge sesenta
+ldi r16, 8
+cp r16, d_high
+brge ochenta
+rjmp cien
+
+; Valores obtenidos en la tabla
+cero:
+ldi r16, 123
+out OCR0, r16
+rjmp to_main
+veinte:
+ldi r16, 127
+out OCR0, r16
+rjmp to_main
+cuarenta:
+ldi r16, 131
+out OCR0, r16
+rjmp to_main
+sesenta:
+ldi r16, 135
+out OCR0, r16
+rjmp to_main
+ochenta:
+ldi r16, 139
+out OCR0, r16
+rjmp to_main
+cien:
+ldi r16, 143
+out OCR0, r16
+rjmp to_main
+
+to_main:
+rcall traba
+rjmp main
+
+retardo:	
+	ldi temporal1, 0xAF
+	ciclo1:
+		dec temporal1
+		breq salir
+		ldi temporal2, 0xAF
+	ciclo2:
+		dec temporal2
+		breq ciclo1
+		rjmp ciclo2 
+salir:
+ret
+
+traba:
+	;Identificar qué pin es el presionado
+	sbis PINA, 6
+	rcall traba6
+	sbis PINA, 5
+	rcall traba5
+	sbis PINA, 4
+	rcall traba4
+	ret
+
+traba6:
+	sbis PINA, 6
+	rjmp traba6
+	rcall retardo
+	sbis PINA, 6
+	rjmp traba6
+	ret
+
+traba5:
+	sbis PINA, 5
+	rjmp traba5
+	rcall retardo
+	sbis PINA, 5
+	rjmp traba5
+	ret
+
+traba4:
+	sbis PINA, 4
+	rjmp traba4
+	rcall retardo
+	sbis PINA, 4
+	rjmp traba4
+	ret
 
 
 ;*********************************
